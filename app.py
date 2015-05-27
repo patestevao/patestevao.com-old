9e284138eca0b2f847a8893c9ff9f31dd8c79f0f
pat-estevao <pat.estevao@live.com>
2015-05-27 15:12:50 -0300

    fixes font, rearranges display, optimizes images and creates contact page

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/index.html')
+def index2():
+	return render_template('index.html')
+
+@app.route('/contact.html')
+def contact():
+	return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
