7c03446d7178f03ac4ededd1f4c10f1695492bec
Pat <patriciaestevao28@gmail.com>
2016-03-22 14:45:02 -0300

    creates CNAME file on freeze script

--- freeze.py
+++ freeze.py
@@ -1,3 +1,4 @@
+import os
 from flask_frozen import Freezer
 from app import app
 
@@ -5,3 +6,5 @@ freezer = Freezer(app)
 
 if __name__ == '__main__':
     freezer.freeze()
+    with open('build/CNAME', 'w') as cname_file:
+        print >>cname_file, 'patestevao.com'
